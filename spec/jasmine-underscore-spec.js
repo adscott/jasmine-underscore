e2e8676061e49e536e549afbd252ceac2e87f281
Adam Scott <ascott@thoughtworks.com>
2011-06-19 12:27:37 +1000

    Added using function.

--- spec/jasmine-underscore-spec.js
+++ spec/jasmine-underscore-spec.js
@@ -1,51 +1,70 @@
 describe('jasmine-underscore', function () {
-  it('should be empty', function () {
-    expect([]).toBeEmpty();	
-  });
+	
+  describe('matchers',function () {
+    it('should be empty', function () {
+      expect([]).toBeEmpty();	
+    });
 
-  it('should be a DOM element', function () {
-    expect(document.body).toBeElement();	
-  });
+    it('should be a DOM element', function () {
+      expect(document.body).toBeElement();	
+    });
 
-  it('should be an array', function () {
-    expect([1]).toBeArray();
-  });
+    it('should be an array', function () {
+      expect([1]).toBeArray();
+    });
 
-  it('should be an arguments array', function () {
-    expect(arguments).toBeArguments();	
-  });
+    it('should be an arguments array', function () {
+      expect(arguments).toBeArguments();	
+    });
 
-  it('should be a function', function () {
-    expect(function () {}).toBeFunction();	
-  });
+    it('should be a function', function () {
+      expect(function () {}).toBeFunction();	
+    });
 
-  it('should be a string', function () {
-    expect('foo').toBeString();	
-  });
+    it('should be a string', function () {
+      expect('foo').toBeString();	
+    });
 
-  it('should be a number', function () {
-    expect(2).toBeNumber();	
-  });
+    it('should be a number', function () {
+      expect(2).toBeNumber();	
+    });
 
-  it('should be a boolean', function () {
-    expect(false).toBeBoolean();	
+    it('should be a boolean', function () {
+      expect(false).toBeBoolean();	
+    });
+  
+    it('should be a date', function () {
+      expect(new Date()).toBeDate();	
+    });
+  
+    it('should be NaN', function () {
+      expect(NaN).toBeNaN();	
+    });
+  
+    it('should be null', function () {
+      expect(null).toBeNull();	
+    });
+  
+    it('should be undefined', function () {
+      expect(undefined).toBeUndefined();	
+    });
   });
   
-  it('should be a date', function () {
-    expect(new Date()).toBeDate();	
+  describe('negated matchers', function () {
+    it('should not be boolean', function () {
+      expect('foo').not.toBeBoolean();
+    });
   });
   
-  it('should be NaN', function () {
-    expect(NaN).toBeNaN();	
+  describe('using', function () {
+    using([1, 2, 3], [2, 3, 5], function (first, second, total) {
+      
+      it('should sum ' + first + ' and ' + second, function () {
+          expect(first + second).toEqual(total);
+      });
+      
+    });
   });
-  
-  it('should be null', function () {
-    expect(null).toBeNull();	
-  });
-  
-  it('should be undefined', function () {
-    expect(undefined).toBeUndefined();	
-  }); 
 });
 
 
